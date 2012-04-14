428de42b635ddb6b42db0ba24bbbeb606745f5e8
Sam Breed <sam@quickleft.com>
2012-04-14 15:21:22 -0600

    refactoring to just use union + connect + director, flatiron was getting in the way. better redis usage and the first jab at user sessions

--- server.js
+++ server.js
@@ -1,76 +1,172 @@
 var
-  fs       = require('fs'),
-  crypto   = require('crypto'),
-  redis    = require('redis'),
-  flatiron = require('flatiron'),
-  ecstatic = require('ecstatic'),
-  path     = require('path'),
-  app      = flatiron.app;
+  fs         = require('fs'),
+  path       = require('path'),
+  crypto     = require('crypto'),
 
-app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
-
-var folders = '/app /assets /dist';
-
-app.use(flatiron.plugins.http, {
-
-  before: [
-    ecstatic(__dirname, '/assets'),
-    ecstatic(__dirname, '/app'),
-    ecstatic(__dirname, '/dist')
-  ]
-
-});
-
-var index = fs.readFileSync(__dirname + '/index.html');
-
-app.router.get('*', function(){
-  this.res.writeHead(200);
-  this.res.end(index);
-});
+  union      = require('union'),
+  director   = require('director'),
+  ecstatic   = require('ecstatic'),
+  connect    = require('connect'),
+  redis      = require('redis'),
+  RedisStore = require('connect-redis')(connect);
 
 var client = redis.createClient();
 
+var router = new director.http.Router();
+
+/* ------------------------------ Middleware ------------------------------ */
+
+var ec = ecstatic( __dirname );
+var static_dirs = /^\/[assets|app|dist]/;
+
+var server = union.createServer({
+
+  before: [
+
+    connect.favicon('./favicon.ico'),
+
+    // conditonally loadstatic middleware with ecstatic
+    function( req, res, next ){
+
+      return static_dirs.test( req.url ) ?
+        ec( req, res, next ) :
+        res.emit('next');
+
+    },
+
+    connect.cookieParser('sgodtohevoli'),
+
+    connect.session({
+      store: new RedisStore(),
+      cookie: { maxAge: +new Date() + 60000 }
+    }),
+
+    function( req, res, next ){
+      if( !router.dispatch(req, res) ){
+        res.emit('next');
+      }
+    }
+  ]
+});
+
+/* ------------------------------ Routes ------------------------------ */
+
 var socket;
+var index = fs.readFileSync('./index.html');
 
-app.router.post('/files', function(){
-  var body = this.req.body;
-  var hash = crypto.createHash('md5');
+router.path(/\/files/, function(){
+  // list
+  this.get(function(){
+    var _this = this;
+    client.smembers('files', function(err, docs){
+      if(err)
+        return _this.res.json(500, { error: err });
 
-  hash.update( body.body + body.name );
-  body.id = hash.digest('hex').slice(0,8);
+      _this.res.json(docs);
+    });
+  });
 
-  client.set(body.id, JSON.stringify(body), redis.print);
+  // list by user
+  this.get(/\/mine/, function(){
+    var _this = this;
+    client.smembers('docs:' + this.req.session.id, function(err, docs){
+      if(err)
+        return _this.res.json(500, { error: err });
 
-  this.res.json(body);
-});
+      _this.res.json(docs);
+    });
+  });
 
-app.router.put('/files/:id', function(){
-  var body = this.req.body;
+  // create
+  this.post(function(){
+    var body = this.req.body;
+    var hash = crypto.createHash('md5');
 
-  client.set(body.id, JSON.stringify(body), redis.print);
+    hash.update( body.body + body.name + this.req.session.id );
+    body.id = hash.digest('hex').slice(0,8);
 
-  if( socket !== undefined ){
-    socket.emit(body.id, body);
-  }
 
-  this.res.json(body);
-});
+    var multi = client.multi();
 
-app.router.get('/files/:id', function( id ){
-  var _this = this;
+    multi.sadd( 'users', this.req.session.id, redis.print );
+    multi.set( body.id, JSON.stringify(body), redis.print );
+    multi.sadd( 'files', body.id, redis.print );
+    multi.sadd( 'docs:' + this.req.session.id, body.id, redis.print );
 
-  client.get(id, function(err, reply){
-    if( reply ) {
-      _this.res.json( JSON.parse( reply.toString() ) );
+    multi.exec();
+
+    this.res.json(201, body);
+  });
+
+  // update
+  this.put(/\/(\w+)/, function(doc){
+    var body = this.req.body;
+
+    if( client.sismember( 'docs:' + this.req.session.id, body.id ) ){
+      client.set( body.id, JSON.stringify(body), redis.print );
+
+      if( socket !== undefined ){
+        socket.emit(body.id, body);
+      }
+
+      this.res.json(body);
     } else {
-      _this.res.json( {}, 404 );
+      this.res.json(403, {
+        error: "You don't have access to write to this file"
+      });
     }
   });
+
+  // delete
+  this['delete'](/\/(\w+)/, function( id ){
+    if( client.sismember( 'docs:' + this.req.session.id, id ) ){
+      client.del( id, redis.print );
+
+      this.res.json(204);
+    } else {
+      this.res.json(403, {
+        error: "You don't have access to write to this file"
+      });
+    }
+  });
+
+  // show
+  this.get(/\/(\w+)/, function( id ){
+    var _this = this;
+
+    client.get( id, function(err, reply){
+      try {
+
+        if( reply ) {
+          _this.res.json( JSON.parse( reply.toString() ) );
+        } else {
+          _this.res.json( 404, { error: 'Not Found' } );
+        }
+
+      } catch( parse_error ) {
+        _this.res.json( 500, { error: parse_error });
+      }
+
+    });
+  });
+
 });
 
-app.start(process.env.PORT || 3000);
+router.get(/\/(.+)?/, function(req, res, next){
+  this.res.html(index);
+});
 
-var io = require('socket.io').listen(app.server);
+
+server.listen(process.env.PORT || 3000);
+
+/* ------------------------------ Sockets ------------------------------ */
+
+var io = require('socket.io').listen(server);
+
+io.enable('browser client minification');
+io.enable('browser client etag');
+io.enable('browser client gzip');
+io.set('log level', 1);
 
 io.sockets.on('connection', function(s) {
   socket = s;
